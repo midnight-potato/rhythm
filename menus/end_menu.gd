75d40cb8abd50be9d64d0dc45dbdfa1ce99b8dcc
Leo Lin <88123479+FroggLeo@users.noreply.github.com>
2026-01-07 00:45:17 -0800

    add end menu labels and buttons

---
+++ menus/end_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var stats = {
+	'combo' = 0,
+	'max_combo' = 0,
+	'misses' = 0,
+	'score' = 0.0
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
